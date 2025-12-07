5a898844c8cd4dfb888abf3681aa700c99579adb
Nambona Adeline <adeline.p@pm.me>
2025-12-07 23:39:30 +0100

    correction netlify.tolm

--- cypress.config.js
+++ cypress.config.js
@@ -4,14 +4,20 @@ module.exports = defineConfig({
   projectId: "smvz27",
 
   e2e: {
-    baseUrl: "http://localhost:3000",
-    supportFile: false,
+    setupNodeEvents(on, config) {
+      // Tu peux ici ajouter d'autres hooks si besoin
+      return config;
+    },
+    baseUrl: "http://localhost:3000", // Ton serveur local pendant les tests
+    specPattern: "cypress/e2e/**/*.cy.js",
   },
 
   component: {
     devServer: {
       framework: "react",
       bundler: "webpack",
+      webpackConfig: require("./webpack.config.js"), // fichier webpack compatible
+
     },
   },
 });
